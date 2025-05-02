b6a6795770f62b4589e70673a3b7edb90165cea8
HJYIMN0 <sim0allaseconda@gmail.com>
2025-05-02 20:34:51 +0200

    Update esercizio 07

--- Assets/Scenes/Scripts/Esercizio07_Script.cs
+++ Assets/Scenes/Scripts/Esercizio07_Script.cs
@@ -7,16 +7,29 @@ public class Esercizio07_Script : MonoBehaviour
     public int dimensioneArray = 10;
     public int valoreMassimo = 100;
     public int valoreMinimo = 20;
+    int[] array = new int[0];
 
-    void GeneraArray()
+    void GeneraArray(int a, int b, int c)
     { 
-        int [] array = new int[dimensioneArray + 1];
+        array = new int[a];
         for (int i = 0; i < array.Length; i++)
         {
-            array[i] = Random.Range(valoreMinimo, valoreMassimo);
-            Debug.Log("Questo è il valore nella posizione " + i + " dell'array (" + array[i] + ")");
+            array[i] = Random.Range(b, c);
+            
         }
+        Debug.Log("Fine funzione GeneraArray");
         
+    }
+
+    void StampaArray(int[] a)
+    {
+        for (int i = 0; i < dimensioneArray; i++) 
+        {
+            Debug.Log("Questo è il valore nella posizione " + i + " dell'array (" + array[i] + ")");
+        
+        }
+        Debug.Log("Fine funzione StampaArray");
+
     }
 
 
@@ -24,7 +37,8 @@ public class Esercizio07_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GeneraArray();
+        GeneraArray(dimensioneArray, valoreMassimo, valoreMinimo);
+        StampaArray(array[dimensioneArray]);
         
     }
 
